7da03aa4d125e6208bbd8af6d9b8a7ec107be39b
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-03-11 23:06:29 +0900

    separate devise:controllers and routing.

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -15,3 +15,4 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+console.log('I am admin/application.js');
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,4 @@
-class Admin::CategoriesController < ApplicationController
-  before_action :authenticate_admin!
-  
+class Admin::CategoriesController < Admin::ApplicationController
   def index
   end
 
