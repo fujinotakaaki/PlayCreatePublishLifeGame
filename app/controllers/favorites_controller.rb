0bbb8e092f9e4b3bd7968a5dc12ea35fa5af8e8a
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-03-18 17:37:45 +0900

    Built up favorite controller and actions

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,13 @@
 class FavoritesController < ApplicationController
   def create
+    favorite = Favorite.new( user_id: current_user.id, pattern_id: params[ :pattern_id ] )
+    favorite.save
+    @pattern_id = params[ :pattern_id ]
   end
 
   def destroy
+    favorite = Favorite.find_by( user_id: current_user.id, pattern_id: params[ :pattern_id ] )
+    favorite.destroy
+    @pattern_id = params[ :pattern_id ]
   end
 end
