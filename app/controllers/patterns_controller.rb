4c212b2c20f6fdaf021f189d05e9ff1674ed5d07
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-03-27 00:51:27 +0900

    Build up keyword searching.

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -24,13 +24,22 @@ class PatternsController < ApplicationController
   end
 
   def index
-    # ページングの設定はconfig/initializers/kaminari_config.rb
-    if  !! params[ :category_id ] then
-      # カテゴリー別表示をする場合
-      @patterns = Pattern.where( category_id: params[ :category_id ] ).page( params[ :page ] ).reverse_order
-    else
-      # 全表示をする場合
-      @patterns = Pattern.page( params[ :page ] ).reverse_order
+    # 一覧画面に表示する項目の条件の取得
+    key, value = search_params
+    # 一覧画面に表示する項目の取得
+    @patterns = begin
+      # 検索条件で分岐
+      case key
+      when search_category? then
+        # カテゴリー検索の場合
+        Pattern.where( category_id: value ).page( params[ :page ] ).reverse_order
+      when search_keyword?( value ) then
+        # キーワード検索の場合
+        Pattern.where( 'name LIKE ? or introduction LIKE ?', "%#{value}%", "%#{value}%" ).page( params[ :page ] ).reverse_order
+      else
+        # 全投稿表示の場合
+        Pattern.page( params[ :page ] ).reverse_order
+      end
     end
   end
 
@@ -44,10 +53,10 @@ class PatternsController < ApplicationController
     @pattern = Pattern.find( params[ :id ] )
     # gonにデータを格納
     set_to_gon( @pattern )
-    # このパターンに対していちばん最近投稿されたコメントをピックアップ
-    @latest_comments = PostComment.order(created_at: :desc).where( pattern_id: params[ :id ] ).limit(5)
+    # このパターンに対し最近投稿されたコメント5件をピックアップ
+    @latest_comments = PostComment.where( pattern_id: params[ :id ] ).reverse_order.limit(5)
     # 最近投稿されたカテゴリが同じパターン2件をピックアップ（自分を除く）
-    @sampling_patterns = Pattern.order(created_at: :desc).where( 'category_id = ? and id != ?', @pattern.category_id, @pattern.id ).limit(2)
+    @sampling_patterns = Pattern.where( 'category_id = ? and id != ?', @pattern.category_id, @pattern.id ).reverse_order.limit(2)
   end
 
   def update
@@ -74,10 +83,29 @@ class PatternsController < ApplicationController
   def create_pattern_params
     params.require( :pattern ).permit( :name, :introduction, :image, :category_id, :display_format_id,
       :margin_top, :margin_bottom, :margin_left, :margin_right, :is_torus, :normalized_rows_sequence )
-  end
+    end
 
-  def update_pattern_params
-    params.require( :pattern ).permit( :name, :introduction, :image, :category_id, :display_format_id,
-      :margin_top, :margin_bottom, :margin_left, :margin_right, :is_torus, :is_secret )
-  end
-end # class
+    def update_pattern_params
+      params.require( :pattern ).permit( :name, :introduction, :image, :category_id, :display_format_id,
+        :margin_top, :margin_bottom, :margin_left, :margin_right, :is_torus, :is_secret )
+      end
+
+      # 一覧表示の項目検索条件取得メソッド
+      def search_params
+        begin
+          params.require( :search ).permit( :category, :keyword ).to_hash.flatten
+        rescue => e
+          # puts e
+        end
+      end
+
+      # カテゴリ検索か判定
+      def search_category?
+        -> key { key&.match?( 'category' ) }
+      end
+
+      # キーワード検索か判定（空文字の場合はfalse）
+      def search_keyword?( value )
+        -> key { ! value.blank? && key&.match?( 'keyword' ) }
+      end
+    end # class
