f2ef042d4e9d381ec74aee90b05387e3c62cfe70
fujinotakaaki <taka0207879576@gmail.com>
2020-05-12 18:51:29 +0900

    [Update] Admin::Categories controller test

--- spec/controllers/admin/categories_spec.rb
+++ spec/controllers/admin/categories_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 # bundle exec rspec spec/controllers/admin/categories_spec.rb
 RSpec.describe Admin::CategoriesController do
+  let!(:pattern){create(:pattern_random)}
   let!(:category){create(:category)}
   let(:user){create(:user)}
   let(:admin){create(:admin)}
@@ -8,53 +9,54 @@ RSpec.describe Admin::CategoriesController do
   let(:attributes_data){attributes_for(:category)}
 
   describe '非ログインユーザの場合' do
-    context 'get #index' do
-      it 'リクエストに失敗' do
+    context 'GET #index' do
+      it 'リクエストが失敗' do
         get :index
         expect(response).to have_http_status 302
       end
     end
 
-    context 'post #create' do
-      it 'リクエストに失敗' do
+    context 'POST #create' do
+      it 'リクエストが失敗' do
         post :create, params: {category: attributes_data}, as: :js
         expect(response).to have_http_status 401
       end
 
-      it 'レコードが作成されないこと' do
+      it 'レコードの登録が失敗' do
         expect do
           post :create, params: {category: attributes_data}, as: :js
         end.to_not change(Category, :count)
       end
     end
 
-    context 'get #edit' do
-      it 'リクエストに失敗' do
+    context 'GET #edit' do
+      it 'リクエストが失敗' do
         get :edit, xhr: true,  params: {id: category}, as: :js
         expect(response).to have_http_status 401
       end
     end
 
-    context 'patch #update' do
-      it 'リクエストに失敗' do
+    context 'PATCH #update' do
+      it 'リクエストが失敗' do
         patch :update, params: {id: category, category: attributes_data}, as: :js
         expect(response).to have_http_status 401
       end
 
-      it 'レコードが更新されないこと' do
+      it 'レコードの更新が失敗' do
         expect(category.name).to_not eq attributes_data[:name]
         patch :update, params: {id: category, category: attributes_data}, as: :js
-        expect(category.reload.name).to_not eq attributes_data[:name]
+        category.reload
+        expect(category.name).to_not eq attributes_data[:name]
       end
     end
 
-    context 'delete #destroy' do
-      it 'リクエストに失敗' do
+    context 'DELETE #destroy' do
+      it 'リクエストが失敗' do
         delete :destroy, params: {id: category}, as: :js
         expect(response).to have_http_status 401
       end
 
-      it 'レコードが削除されないこと' do
+      it 'レコードの削除が失敗' do
         expect do
           delete :destroy, params: {id: category}, as: :js
         end.to_not change(Category, :count)
@@ -68,53 +70,54 @@ RSpec.describe Admin::CategoriesController do
       sign_in user
     end
 
-    context 'get #index' do
-      it 'リクエストに失敗' do
+    context 'GET #index' do
+      it 'リクエストが失敗' do
         get :index
         expect(response).to have_http_status 302
       end
     end
 
-    context 'post #create' do
-      it 'リクエストに失敗' do
+    context 'POST #create' do
+      it 'リクエストが失敗' do
         post :create, params: {category: attributes_data}, as: :js
         expect(response).to have_http_status 401
       end
 
-      it 'レコードが作成されないこと' do
+      it 'レコードの登録が失敗' do
         expect do
           post :create, params: {category: attributes_data}, as: :js
         end.to_not change(Category, :count)
       end
     end
 
-    context 'get #edit' do
-      it 'リクエストに失敗' do
+    context 'GET #edit' do
+      it 'リクエストが失敗' do
         get :edit, xhr: true,  params: {id: category}, as: :js
         expect(response).to have_http_status 401
       end
     end
 
-    context 'patch #update' do
-      it 'リクエストに失敗' do
+    context 'PATCH #update' do
+      it 'リクエストが失敗' do
         patch :update, params: {id: category, category: attributes_data}, as: :js
         expect(response).to have_http_status 401
       end
 
-      it 'レコードが更新されないこと' do
+      it 'レコードの更新が失敗' do
         expect(category.name).to_not eq attributes_data[:name]
         patch :update, params: {id: category, category: attributes_data}, as: :js
-        expect(category.reload.name).to_not eq attributes_data[:name]
+        category.reload
+        expect(category.name).to_not eq attributes_data[:name]
       end
     end
 
-    context 'delete #destroy' do
-      it 'リクエストに失敗' do
+    context 'DELETE #destroy' do
+      it 'リクエストが失敗' do
         delete :destroy, params: {id: category}, as: :js
         expect(response).to have_http_status 401
       end
 
-      it 'レコードが削除されないこと' do
+      it 'レコードの削除が失敗' do
         expect do
           delete :destroy, params: {id: category}, as: :js
         end.to_not change(Category, :count)
@@ -127,56 +130,63 @@ RSpec.describe Admin::CategoriesController do
       sign_in admin
     end
 
-    context 'get #index' do
-      it 'リクエストに成功' do
+    context 'GET #index' do
+      it 'リクエストが成功' do
         get :index
         expect(response).to have_http_status 200
       end
     end
 
-    context 'post #create' do
-      it 'リクエストに成功' do
+    context 'POST #create' do
+      it 'リクエストが成功' do
         post :create, params: {category: attributes_data}, as: :js
         expect(response).to have_http_status 200
       end
 
-      it 'レコードの作成に成功' do
+      it 'レコードの登録が成功' do
         expect do
           post :create, params: {category: attributes_data}, as: :js
         end.to change(Category, :count).by(1)
       end
     end
 
-    context 'get #edit', focus: true do
-      it 'リクエストに成功' do
+    context 'GET #edit' do
+      it 'リクエストが成功' do
         get :edit, xhr: true,  params: {id: category}, as: :js
         expect(response).to have_http_status 200
       end
     end
 
-    context 'patch #update' do
-      it 'リクエストに成功' do
+    context 'PATCH #update' do
+      it 'リクエストが成功' do
         patch :update, params: {id: category, category: attributes_data}, as: :js
         expect(response).to have_http_status 200
       end
 
-      it 'レコードの更新に成功' do
+      it 'レコードの更新が成功' do
         expect(category.name).to_not eq attributes_data[:name]
         patch :update, params: {id: category, category: attributes_data}, as: :js
-        expect(category.reload.name).to eq attributes_data[:name]
+        category.reload
+        expect(category.name).to eq attributes_data[:name]
       end
     end
 
-    context 'delete #destroy' do
-      it 'リクエストに成功' do
+    context 'DELETE #destroy' do
+      it 'リクエストが成功' do
         delete :destroy, params: {id: category}, as: :js
         expect(response).to have_http_status 200
       end
 
-      it 'レコードの削除に成功' do
+      it 'レコードの削除が成功' do
         expect do
           delete :destroy, params: {id: category}, as: :js
         end.to change(Category, :count).by(-1)
+      end
+
+      it 'パターンに関連付けされているレコードの削除が失敗' do
+        expect do
+          delete :destroy, params: {id: pattern.category}, as: :js
+        end.to_not change(Category, :count)
       end
     end
   end # describe '管理者の場合'
